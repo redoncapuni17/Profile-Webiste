1bd53224cc57880e57dbc584a96e3c83ff88d103
redon <redoncapuni@gmail.com>
2023-07-29 17:23:43 +0300

    First Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabContents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabContent of tabContents) {
+    tabContent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
+
+/*------------------google sheet----------------*/
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzQ_cA-7Ew1hMyoFDIsC7LkM22CcYQLzE4yTwt99cv6sMK6l-iyJDgHWX8BPWKFMZ7iWw/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message Sent Successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
